0a98ebafa9c43cebae90db2c7e4f8c7007ce5039
GenisGenerik <164584630+GenisGenerik@users.noreply.github.com>
2024-10-16 22:55:50 +0700

    Tugas Praktikum

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ btnTambah.onclick = (e) => {
    }
    let task = document.createElement("div");
    task.id = `${Date.now()}`;
-   task.className = `...`;
+   task.className = `w-1/4 flex justify-center mt-5`;
    task.innerHTML = `
-         <p class="...">${title}</p>
+         <p class="w-1/2">${title}</p>
          <input type="button" id="btn-selesai" value="SELESAI" data-task="${task.id}"
-                class="...">
+                class=" bg-green-400 rounded-md font-bold px-2">
          <input type="button" id="btn-hapus" value="HAPUS" data-task="${task.id}"
-                class="...">
+                class="ml-6  bg-red-500 rounded-md font-bold px-2">
    `;
    output.appendChild(task);
    input.value = "";
